b716d10b46710db13cba2fd9e30cfda975477fac
Sergey Sedelnikov <sergey.sedelnikov@altal.fi>
2016-02-12 14:38:59 +0200

    map section refactored

--- src/LunchSelection/Source/Modules/Sections/mapSection.js
+++ src/LunchSelection/Source/Modules/Sections/mapSection.js
@@ -7,37 +7,43 @@ export class MapSection{
     this.element = Element;
   }
 
-  attached(){
-
-    //generate map
-    let map = new google.maps.Map($(this.element).find('#map')[0],{
-      center: {lat: 44.540, lng: -78.546},
-      zoom: 16
-    });
-
-    //get current position
-    if(navigator.geolocation){
-
-        navigator.geolocation.getCurrentPosition(position => {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-
-          //set map center
-          map.setCenter(pos);
-          //set current location
-          let icon = {
-            url: '../content/images/me.png',
-            anchor: new google.maps.Point(16, 16),
-            origin: new google.maps.Point(0, 0),
-          };
-          let meMarker = new google.maps.Marker({
-                                position: pos,
-                                map: map,
-                                icon: icon
-                              });
+  //initialize map
+  initMap() {
+      //generate map
+      let map = new google.maps.Map($(this.element).find('#map')[0],{
+        center: {lat: 44.540, lng: -78.546},
+        zoom: 16
       });
-    }
+
+      //get current position
+      if(navigator.geolocation){
+
+          navigator.geolocation.getCurrentPosition(position => {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            //set map center
+            map.setCenter(pos);
+            //set current location
+            let icon = {
+              url: '../content/images/me.png',
+              anchor: new google.maps.Point(16, 16),
+              origin: new google.maps.Point(0, 0),
+            };
+            let meMarker = new google.maps.Marker({
+                                  position: pos,
+                                  map: map,
+                                  icon: icon
+                                });
+        });
+      }
   }
+
+  //on element attached to the screen
+  attached(){
+    this.initMap();
+  }
+
 }
