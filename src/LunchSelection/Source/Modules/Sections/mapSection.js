2836e638b951c988365e64ae3255cdd605fe4246
Sergey Sedelnikov <sergey.sedelnikov@altal.fi>
2016-02-12 14:31:09 +0200

    position is detected of the current user

--- src/LunchSelection/Source/Modules/Sections/mapSection.js
+++ src/LunchSelection/Source/Modules/Sections/mapSection.js
@@ -8,10 +8,36 @@ export class MapSection{
   }
 
   attached(){
-    var map = new google.maps.Map($(this.element).find('#map')[0],{
-      center: {lat: 44.540, lng: -78.546},
-      zoom: 8
-    });
-  }
 
+    //generate map
+    let map = new google.maps.Map($(this.element).find('#map')[0],{
+      center: {lat: 44.540, lng: -78.546},
+      zoom: 16
+    });
+
+    //get current position
+    if(navigator.geolocation){
+
+        navigator.geolocation.getCurrentPosition(position => {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          //set map center
+          map.setCenter(pos);
+          //set current location
+          let icon = {
+            url: '../content/images/me.png',
+            anchor: new google.maps.Point(16, 16),
+            origin: new google.maps.Point(0, 0),
+          };
+          let meMarker = new google.maps.Marker({
+                                position: pos,
+                                map: map,
+                                icon: icon
+                              });
+      });
+    }
+  }
 }
